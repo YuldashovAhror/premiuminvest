e39a8be21d24ff940d417c973bbbe4151c60b20e
Ozodbek Juraev <ozodbekmcfly@gmail.com>
2022-07-28 16:10:23 +0500

    Initial commit

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
+use App\Models\EmployeeCompany;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class EmployeeController extends Controller
 {
@@ -19,7 +22,24 @@ class EmployeeController extends Controller
     
     public function store(Request $request)
     {
-        dd($request->all());
-        return view('admin.employee.create');
+        $employee = New Employee();
+        $employee->type = $request->type;
+        $employee->name_uz = $request->name_uz;
+        $employee->name_ru = $request->name_ru;
+        $employee->name_en = $request->name_en;
+        $employee->save();
+
+        if($request->companies){
+            foreach($request->companies as $req_company){
+                $company = New EmployeeCompany();
+                $company->employee_id = $employee->id;
+                $imageName = Str::random(10).'.'.$req_company->extension();
+                $req_company->move(public_path('images/employee'), $imageName);
+                $company->photo = 'images/employee/'.$imageName;
+                $company->save();
+            }
+        }
+
+        return view('admin.employee.index');
     }
 }
--- lang/uz/asd.php
+++ lang/uz/asd.php
@@ -1,6 +1,6 @@
 <?php
 
-
+$arr = [];
 foreach(App\Models\Words::orderBy('id')->get() as $word)
 {
     $arr[$word->key] = $word->uz;
