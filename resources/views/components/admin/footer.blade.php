ae2aea03fd4fc2f60d347f256ea16c09c3c5e1e9
EBirodar97 <92210441+EBirodar@users.noreply.github.com>
2022-07-29 18:31:06 +0500

    footer change

--- resources/views/components/admin/footer.blade.php
+++ resources/views/components/admin/footer.blade.php
@@ -9,7 +9,7 @@
       <div class="col-sm-6">
         <div class="text-sm-end d-none d-sm-block">
           Created with <i class="mdi mdi-heart text-danger"></i> by <a href="../../index.html" target="_blank"
-            class="text-reset">Ozodbek</a>
+            class="text-reset">Birodar</a>
         </div>
       </div>
     </div>
